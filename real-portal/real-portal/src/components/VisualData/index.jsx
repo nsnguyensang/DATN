3470dfb66017b6ffd03d9fb45eede18d72774102
Nguyễn Văn Sáng <nguyensanggb@gmail.com>
2023-07-06 18:23:43 +0700

    update version charjs vs react-chartjs-2

--- real-portal/real-portal/src/components/VisualData/index.jsx
+++ real-portal/real-portal/src/components/VisualData/index.jsx
@@ -1,10 +1,59 @@
 import React, { Fragment } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const VisualData = () => {
-  return <Fragment>
-    <div>Biểu diễn dữ liệu</div>
-    
-  </Fragment>;
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
+  };
+
+  return (
+    <Fragment>
+      <div>Biểu diễn dữ liệu</div>
+      <Line options={options} data={data} />
+    </Fragment>
+  );
 };
 
 export default VisualData;
