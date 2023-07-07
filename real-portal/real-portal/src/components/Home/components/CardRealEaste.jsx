bd3b303bdcc5115de149bfab4a99b41744ccba08
Nguyễn Văn Sáng <nguyensanggb@gmail.com>
2023-07-08 02:49:53 +0700

    update modal view detail

--- real-portal/real-portal/src/components/Home/components/CardRealEaste.jsx
+++ real-portal/real-portal/src/components/Home/components/CardRealEaste.jsx
@@ -27,7 +27,9 @@ import {
   ButtonGroup,
   ButtomView,
 } from "./style";
+import DetailRealEaste from "./DetailRealEaste";
 const CardRealEaste = ({ data }) => {
+  const [showModalDetail, setShowModalDetail] = useState(false);
   const pricePerProcess = (idx) => {
     let pricePer = (idx?.price / (parseInt(idx?.square) * 1000000)).toFixed(2);
     if (pricePer > 1000000) {
@@ -45,101 +47,130 @@ const CardRealEaste = ({ data }) => {
     }
     return number.toString();
   }
-  return (
-    <CardFull>
-      <CardImage>
-        <img
-          style={{
-            maxWidth: "100%",
-            maxHeight: "100%",
-            objectFit: "contain",
-          }}
-          // src={data.link_image}
-          src="https://alomuabannhadat.vn/watermark/static1.alomuabannhadat.vn/tindang/2022/11/mua-ban-nha-dat-theo-quan-khu-vuc-1667581665-alomuabannhadat.vn.jpg"
-        />
-      </CardImage>
-      <CardInfo>
-        <CardInfoContent>
-          <CardTitle>
-            {data.title}
-            {data.phone_contact !== "" && (
-              <MobileOnCard>
-                {" "}
-                {Number.isInteger(data.phone_contact)
-                  ? "0" + data.phone_contact
-                  : data.phone_contact}
-              </MobileOnCard>
-            )}
-          </CardTitle>
+  const fallbackImage =
+    "https://file4.batdongsan.com.vn/resize/1275x717/2023/04/05/20230405163430-2777_wm.jpg";
 
-          <div>
-            <CardConfig>
-              <CardConfigPrice>
-                {formatNumber(parseInt(data.price))}
-              </CardConfigPrice>
-              <CardConfigDot>.</CardConfigDot>
-              <CardConfigArea>{Math.floor(data.square)} m²</CardConfigArea>
-              <CardConfigDot>.</CardConfigDot>
-              <CardConfigPricePerM2>
-                {pricePerProcess(data)} tr/m²
-              </CardConfigPricePerM2>
-              <CardConfigDot>.</CardConfigDot>
-              <CardRoom>
-                <Tooltip
-                  placement="bottom"
-                  title={`${
-                    data?.bedroom !== "0" ? data?.bedroom : "1"
-                  } Phòng ngủ`}
+  const handleImageError = (event) => {
+    event.target.src = fallbackImage;
+  };
+  return (
+    <Fragment>
+      <CardFull>
+        <CardImage>
+          <img
+            style={{
+              maxWidth: "100%",
+              maxHeight: "100%",
+              objectFit: "contain",
+            }}
+            src={data.link_image}
+            onError={handleImageError}
+          />
+        </CardImage>
+        <CardInfo>
+          <CardInfoContent>
+            <CardTitle>
+              {data.title}
+              {data.phone_contact !== "" && (
+                <MobileOnCard>
+                  {" "}
+                  {Number.isInteger(data.phone_contact)
+                    ? "0" + data.phone_contact
+                    : data.phone_contact}
+                </MobileOnCard>
+              )}
+            </CardTitle>
+
+            <div>
+              <CardConfig>
+                <CardConfigPrice>
+                  {formatNumber(parseInt(data.price))}
+                </CardConfigPrice>
+                <CardConfigDot>.</CardConfigDot>
+                <CardConfigArea>{Math.floor(data.square)} m²</CardConfigArea>
+                <CardConfigDot>.</CardConfigDot>
+                <CardConfigPricePerM2>
+                  {pricePerProcess(data)} tr/m²
+                </CardConfigPricePerM2>
+                <CardConfigDot>.</CardConfigDot>
+                <CardRoom>
+                  <Tooltip
+                    placement="bottom"
+                    title={`${
+                      data?.bedroom !== "0" ? data?.bedroom : "1"
+                    } Phòng ngủ`}
+                  >
+                    <CardRoomSpan>
+                      {data?.bedroom !== "0" ? data?.bedroom : "1"}
+                    </CardRoomSpan>
+                    <FontAwesomeIcon
+                      icon={faBed}
+                      style={{ color: "#707070" }}
+                    />
+                  </Tooltip>
+                </CardRoom>
+                <CardConfigDot>.</CardConfigDot>
+                <CardRoom>
+                  <Tooltip
+                    placement="bottom"
+                    title={`${
+                      data?.bathroom !== "0" ? data?.bathroom : "1"
+                    } WC`}
+                  >
+                    <CardRoomSpan>
+                      {data?.bathroom !== "0" ? data?.bathroom : "1"}
+                    </CardRoomSpan>
+                    <FontAwesomeIcon
+                      icon={faBath}
+                      style={{ color: "#707070" }}
+                    />
+                  </Tooltip>
+                </CardRoom>
+                <CardConfigDot>.</CardConfigDot>
+                <CardLocation>
+                  {data.district}, {data.province}
+                </CardLocation>
+              </CardConfig>
+            </div>
+            <CardDescription>
+              {data.description.slice(0, 200)}...
+            </CardDescription>
+          </CardInfoContent>
+          <CardContact>
+            <CardPublishedInfo>
+              <CardAvatar>
+                <ProfileAvatarNoPhoto>
+                  {data.name_contact.split(" ").pop().charAt(0)}
+                </ProfileAvatarNoPhoto>
+              </CardAvatar>
+              <ProfileName>&lrm;{data.name_contact}</ProfileName>
+              <ProfileTimeUpdate>
+                <span>{data.date}</span>
+              </ProfileTimeUpdate>
+            </CardPublishedInfo>
+            <ButtonGroup>
+              <Space wrap>
+                <ButtomView onClick={() => setShowModalDetail(true)}>
+                  Xem thêm
+                </ButtomView>
+                <ButtomView
+                  onClick={() => window.open(data.url_page, "_blank")}
                 >
-                  <CardRoomSpan>
-                    {data?.bedroom !== "0" ? data?.bedroom : "1"}
-                  </CardRoomSpan>
-                  <FontAwesomeIcon icon={faBed} style={{ color: "#707070" }} />
-                </Tooltip>
-              </CardRoom>
-              <CardConfigDot>.</CardConfigDot>
-              <CardRoom>
-                <Tooltip
-                  placement="bottom"
-                  title={`${data?.bathroom !== "0" ? data?.bathroom : "1"} WC`}
-                >
-                  <CardRoomSpan>
-                    {data?.bathroom !== "0" ? data?.bathroom : "1"}
-                  </CardRoomSpan>
-                  <FontAwesomeIcon icon={faBath} style={{ color: "#707070" }} />
-                </Tooltip>
-              </CardRoom>
-              <CardConfigDot>.</CardConfigDot>
-              <CardLocation>
-                {data.district}, {data.province}
-              </CardLocation>
-            </CardConfig>
-          </div>
-          <CardDescription>{data.description.slice(0, 200)}...</CardDescription>
-        </CardInfoContent>
-        <CardContact>
-          <CardPublishedInfo>
-            <CardAvatar>
-              <ProfileAvatarNoPhoto>
-                {data.name_contact.split(" ").pop().charAt(0)}
-              </ProfileAvatarNoPhoto>
-            </CardAvatar>
-            <ProfileName>&lrm;{data.name_contact}</ProfileName>
-            <ProfileTimeUpdate>
-              <span>{data.date}</span>
-            </ProfileTimeUpdate>
-          </CardPublishedInfo>
-          <ButtonGroup>
-            <Space wrap>
-              <ButtomView>Xem thêm</ButtomView>
-              <ButtomView onClick={() => window.open(data.url_page, "_blank")}>
-                Bài viết gốc
-              </ButtomView>
-            </Space>
-          </ButtonGroup>
-        </CardContact>
-      </CardInfo>
-    </CardFull>
+                  Bài viết gốc
+                </ButtomView>
+              </Space>
+            </ButtonGroup>
+          </CardContact>
+        </CardInfo>
+      </CardFull>
+      {showModalDetail && (
+        <DetailRealEaste
+          data={data}
+          isOpen={showModalDetail}
+          onClose={() => setShowModalDetail(false)}
+        />
+      )}
+    </Fragment>
   );
 };
 
--- real-portal/real-portal/src/components/Home/components/DetailRealEaste.jsx
+++ real-portal/real-portal/src/components/Home/components/DetailRealEaste.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { LayoutCenter } from "../style";
-import { Carousel } from "antd";
+import { Carousel, Drawer } from "antd";
 import {
   AddressDetail,
   ImageCarousel,
@@ -18,136 +18,184 @@ import {
   InfoOtherItem,
   InfoOtherValue,
 } from "./style";
-const DetailRealEaste = () => {
+
+const DetailRealEaste = ({ data, isOpen, onClose }) => {
+  const pricePerProcess = (idx) => {
+    let pricePer = (idx?.price / (parseInt(idx?.square) * 1000000)).toFixed(2);
+    if (pricePer > 1000000) {
+      pricePer = (pricePer / 1000000).toFixed(2);
+    }
+    return pricePer;
+  };
+  function formatNumber(number) {
+    if (number >= 1000000000000000) {
+      return number / 1000000000000000 + " tỷ";
+    } else if (number >= 1000000000) {
+      return (number / 1000000000).toFixed(2) + " tỷ";
+    } else if (number >= 1000000) {
+      return (number / 1000000).toFixed(2) + " triệu";
+    }
+    return number.toString();
+  }
+  const fallbackImage =
+    "https://file4.batdongsan.com.vn/resize/1275x717/2023/04/05/20230405163430-2777_wm.jpg";
+
+  const handleImageError = (event) => {
+    event.target.src = fallbackImage;
+  };
   return (
-    <LayoutCenter>
-      <Carousel autoplay>
-        <div>
-          <ImageCarousel>
-            <img
-              style={{
-                maxWidth: "100%",
-                maxHeight: "100%",
-                objectFit: "contain",
-              }}
-              src="https://file4.batdongsan.com.vn/resize/1275x717/2023/04/21/20230421122954-58b5_wm.jpg"
-            />
-          </ImageCarousel>
-        </div>
-        <div>
-          <ImageCarousel>2</ImageCarousel>
-        </div>
-        <div>
-          <ImageCarousel>3</ImageCarousel>
-        </div>
-      </Carousel>
-      <div className="web-detail">
-        <TitleDetail>
-          Tổng hợp bảng hàng quỹ căn bán giá rẻ tại dự án Mipec Rubik 360 Xuân
-          Thủy Cầu Giấy
-        </TitleDetail>
-        <AddressDetail>
-          Dự án Mipec Rubik 360, Đường Xuân Thủy, Phường Dịch Vọng Hậu, Cầu
-          Giấy, Hà Nội
-        </AddressDetail>
-        <ShortInfo>
-          <ShortInfoItem>
-            <ShortInfoTitle>Mức giá</ShortInfoTitle>
-            <ShortInfoValue>6,5 tỷ</ShortInfoValue>
-            <ShortInfoExt>~58,56 triệu/m²</ShortInfoExt>
-          </ShortInfoItem>
-          <ShortInfoItem left="64px">
-            <ShortInfoTitle>Diện tích</ShortInfoTitle>
-            <ShortInfoValue>111 m²</ShortInfoValue>
-          </ShortInfoItem>
-          <ShortInfoItem left="64px">
-            <ShortInfoTitle>Phòng ngủ</ShortInfoTitle>
-            <ShortInfoValue>3 PN</ShortInfoValue>
-          </ShortInfoItem>
-          <div style={{ position: "absolute", right: "0", marginTop: "7px" }}>
-            <ButtomView>Bài viết gốc</ButtomView>
-          </div>
-        </ShortInfo>
-        <DescriptionDetail>
-          <DescriptionTitle>Thông tin mô tả</DescriptionTitle>
-          <DescriptionContent>
-            Em Được chuyên bán chuyển nhượng căn hộ tại chung cư dự án Mipec
-            Rubik 360 Xuân Thủy - Cầu Giấy! Đảm bảo và cam kết sẽ hỗ trợ tới quý
-            Khách Hàng lựa chọn được những căn hộ phù hợp nhất nhằm rút ngắn
-            thời gian công sức đi tìm nhà của quý anh chị khách hàng. Bên em sẵn
-            pass của tất cả các căn hộ đang bán. Hỗ trợ quý anh chị khách hàng
-            xem nhà 24/24. LH 0933 929 ***. Hỗ trợ khách hàng với phương châm:
-            Chuyên nghiệp, tận tâm, uy tín. Có xe đưa đón khách hàng đến tham
-            quan dự án. Kết nối trực tiếp với chủ nhà để trao đổi và đàm phán
-            thương lượng giá tốt nhất tới Qúy Khách. Em cập nhật bảng giá mới
-            nhất 02/07/2023: - Căn 2PN + 1VS, diện tích 54m² giá từ 3,3 tỷ. -
-            Căn 2PN + 2VS, diện tích 70m² giá từ 4 tỷ. - Căn 2PN + 2VS, diện
-            tích 77m² giá từ 3,9 tỷ. - Căn 2PN + 2VS, diện tích 83m² giá từ 4,4
-            tỷ. - Căn 3PN + 2VS, diện tích 95m² giá từ 5,3 tỷ. - Căn 3PN + 2VS,
-            diện tích 109m² giá từ 6,2 tỷ. - Căn 4PN + 3VS, diện tích 123m² giá
-            từ 7,9 tỷ.
-          </DescriptionContent>
-        </DescriptionDetail>
-        <DescriptionDetail>
-          <DescriptionTitle>Đặc điểm bất động sản</DescriptionTitle>
+    <Drawer
+      title="Thông tin chi tiết"
+      placement="right"
+      onClose={onClose}
+      open={isOpen}
+      width={800}
+    >
+      <LayoutCenter>
+        <Carousel autoplay>
           <div>
-            <InfoOther>
-              <InfoOtherItem>Diện tích</InfoOtherItem>
-              <InfoOtherValue>111 m²</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Mức giá</InfoOtherItem>
-              <InfoOtherValue>6,5 tỷ</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Hướng nhà</InfoOtherItem>
-              <InfoOtherValue>Bắc</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Hướng ban công</InfoOtherItem>
-              <InfoOtherValue>Nam</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Số phòng ngủ</InfoOtherItem>
-              <InfoOtherValue>3 phòng</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Số toilet</InfoOtherItem>
-              <InfoOtherValue>2 phòng</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Pháp lý</InfoOtherItem>
-              <InfoOtherValue>Sổ đỏ/Sổ hồng</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Dự án</InfoOtherItem>
-              <InfoOtherValue>Vinhome Ocean Park</InfoOtherValue>
-            </InfoOther>
+            <ImageCarousel>
+              <img
+                style={{
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                  objectFit: "contain",
+                }}
+                src={data.link_image}
+                onError={handleImageError}
+              />
+            </ImageCarousel>
           </div>
-        </DescriptionDetail>
-        <DescriptionDetail>
-          <DescriptionTitle>Thông tin liên hệ</DescriptionTitle>
           <div>
-            <InfoOther>
-              <InfoOtherItem>Người đăng</InfoOtherItem>
-              <InfoOtherValue>Đặng Được</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Số điện thoại</InfoOtherItem>
-              <InfoOtherValue>0933 929 555</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Ngày đăng</InfoOtherItem>
-              <InfoOtherValue>06/07/2023</InfoOtherValue>
-            </InfoOther>
-            <InfoOther>
-              <InfoOtherItem>Email</InfoOtherItem>
-              <InfoOtherValue>dangduoc@gmail.com</InfoOtherValue>
-            </InfoOther>
+            <ImageCarousel>2</ImageCarousel>
           </div>
-        </DescriptionDetail>
-      </div>
-    </LayoutCenter>
+          <div>
+            <ImageCarousel>3</ImageCarousel>
+          </div>
+        </Carousel>
+        <div className="web-detail">
+          <TitleDetail>{data.title}</TitleDetail>
+          <AddressDetail>
+            Dự án Mipec Rubik 360, Đường Xuân Thủy, Phường Dịch Vọng Hậu, Cầu
+            Giấy, Hà Nội
+          </AddressDetail>
+          <ShortInfo>
+            <ShortInfoItem>
+              <ShortInfoTitle>Mức giá</ShortInfoTitle>
+              <ShortInfoValue>
+                {formatNumber(parseInt(data.price))}
+              </ShortInfoValue>
+              <ShortInfoExt>~ {pricePerProcess(data)} triệu/m²</ShortInfoExt>
+            </ShortInfoItem>
+            <ShortInfoItem left="64px">
+              <ShortInfoTitle>Diện tích</ShortInfoTitle>
+              <ShortInfoValue>{Math.floor(data.square)} m²</ShortInfoValue>
+            </ShortInfoItem>
+            <ShortInfoItem left="64px">
+              <ShortInfoTitle>Phòng ngủ</ShortInfoTitle>
+              <ShortInfoValue>
+                {data?.bedroom !== "0" ? data?.bedroom : "1"} PN
+              </ShortInfoValue>
+            </ShortInfoItem>
+            <div style={{ position: "absolute", right: "0", marginTop: "7px" }}>
+              <ButtomView onClick={() => window.open(data.url_page, "_blank")}>
+                Bài viết gốc
+              </ButtomView>
+            </div>
+          </ShortInfo>
+          <DescriptionDetail>
+            <DescriptionTitle>Thông tin mô tả</DescriptionTitle>
+            <DescriptionContent>{data.description}</DescriptionContent>
+          </DescriptionDetail>
+          <DescriptionDetail>
+            <DescriptionTitle>Đặc điểm bất động sản</DescriptionTitle>
+            <div>
+              <InfoOther>
+                <InfoOtherItem>Diện tích</InfoOtherItem>
+                <InfoOtherValue>{Math.floor(data.square)} m²</InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Mức giá</InfoOtherItem>
+                <InfoOtherValue>{pricePerProcess(data)} tỷ</InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Chiều rộng</InfoOtherItem>
+                <InfoOtherValue>
+                  {data?.width !== "0" || data?.length !== ""? data?.width : "Không xác định"}
+                </InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Chiều dài</InfoOtherItem>
+                <InfoOtherValue>
+                  {data?.length !== "0" || data?.length !== ""
+                    ? data?.length
+                    : "Không xác định"}
+                </InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Hướng nhà</InfoOtherItem>
+                <InfoOtherValue>
+                  {data?.direct !== "None" ? data?.direct : "Không xác định"}
+                </InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Số tầng</InfoOtherItem>
+                <InfoOtherValue>
+                  {data?.floor !== "0 t"
+                    ? data?.floor.replace(" t", "")
+                    : "Không xác định"}
+                </InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Số phòng ngủ</InfoOtherItem>
+                <InfoOtherValue>
+                  {data?.bedroom !== "0" ? data?.bedroom : "1"} phòng
+                </InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Số toilet</InfoOtherItem>
+                <InfoOtherValue>
+                  {data?.bathroom !== "0" ? data?.bathroom : "1"} phòng
+                </InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Pháp lý</InfoOtherItem>
+                <InfoOtherValue>{data?.juridical}</InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Dự án</InfoOtherItem>
+                <InfoOtherValue>
+                  {data?.project ? data?.project : "Không xác định"}
+                </InfoOtherValue>
+              </InfoOther>
+            </div>
+          </DescriptionDetail>
+          <DescriptionDetail>
+            <DescriptionTitle>Thông tin liên hệ</DescriptionTitle>
+            <div>
+              <InfoOther>
+                <InfoOtherItem>Người đăng</InfoOtherItem>
+                <InfoOtherValue>{data.name_contact}</InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Số điện thoại</InfoOtherItem>
+                <InfoOtherValue>{data.phone_contact}</InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Ngày đăng</InfoOtherItem>
+                <InfoOtherValue>{data.date}</InfoOtherValue>
+              </InfoOther>
+              <InfoOther>
+                <InfoOtherItem>Email</InfoOtherItem>
+                <InfoOtherValue>
+                  {data?.email ? data?.email : "Không xác định "}
+                </InfoOtherValue>
+              </InfoOther>
+            </div>
+          </DescriptionDetail>
+        </div>
+      </LayoutCenter>
+    </Drawer>
   );
 };
 
