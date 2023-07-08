357d8825379bf775982e01035a7fb64938e8d066
Nguyễn Văn Sáng <nguyensanggb@gmail.com>
2023-07-09 02:42:28 +0700

    update ui predict

--- real-portal/real-portal/src/components/Predict/index.jsx
+++ real-portal/real-portal/src/components/Predict/index.jsx
@@ -1,149 +1,272 @@
-import React, { Fragment } from "react";
-import { LayoutPredict, TitlePredict } from "./styles";
-import { Button, Form, Input, InputNumber, Select } from "antd";
+import React, { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import { ReloadOutlined } from "@ant-design/icons";
+import {
+  LayoutPredict,
+  TitlePredict,
+  PredictBox,
+  ButtomView,
+  ResultBox,
+  RecommentBox,
+} from "./styles";
+import {
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Select,
+  Row,
+  Col,
+  Space,
+} from "antd";
 const { Option } = Select;
 
 const Predict = () => {
   const [form] = Form.useForm();
+  //api tỉnh thành, quận huyện
+  const [cities, setCities] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  const [wards, setWards] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedDistrict, setSelectedDistrict] = useState("");
+  const [selectedWard, setSelectedWard] = useState("");
+  useEffect(() => {
+    callAPI("https://provinces.open-api.vn/api/?depth=1");
+  }, []);
+
+  const callAPI = (api) => {
+    axios.get(api).then((response) => {
+      setCities(response.data);
+    });
+  };
+
+  const callApiDistrict = (api) => {
+    axios.get(api).then((response) => {
+      setDistricts(response.data.districts);
+    });
+  };
+
+  const callApiWard = (api) => {
+    axios.get(api).then((response) => {
+      setWards(response.data.wards);
+    });
+  };
+
+  const handleCityChange = (value, option) => {
+    if (value !== undefined) {
+      setSelectedDistrict("");
+      setSelectedWard("");
+      const selectedCityId = value;
+      setSelectedCity(option.children);
+      callApiDistrict(
+        `https://provinces.open-api.vn/api/p/${selectedCityId}?depth=2`
+      );
+    }
+  };
+
+  const handleDistrictChange = (value, option) => {
+    if (value !== undefined) {
+      setSelectedWard("");
+      const selectedDistrictId = value;
+      setSelectedDistrict(option.children);
+      callApiWard(
+        `https://provinces.open-api.vn/api/d/${selectedDistrictId}?depth=2`
+      );
+    }
+  };
+
+  const handleWardChange = (value, option) => {
+    if (value !== undefined) {
+      setSelectedWard(option.children);
+    }
+  };
   const onFinish = (values) => {
     console.log(values);
   };
   const onReset = () => {
     form.resetFields();
   };
-  const onGenderChange = (value) => {
-    switch (value) {
-      case "male":
-        form.setFieldsValue({
-          note: "Hi, man!",
-        });
-        break;
-      case "female":
-        form.setFieldsValue({
-          note: "Hi, lady!",
-        });
-        break;
-      case "other":
-        form.setFieldsValue({
-          note: "Hi there!",
-        });
-        break;
-      default:
-    }
-  };
+
   return (
     <Fragment>
-      <LayoutPredict>
-        <TitlePredict>Dự đoán giá chung cư</TitlePredict>
-        <Form
-          layout="vertical"
-          form={form}
-          name="control-hooks"
-          onFinish={onFinish}
-          style={{
-            maxWidth: 600,
-          }}
-        >
-          <Form.Item
-            name="Diện tích"
-            label="Diện tích"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <InputNumber />
-          </Form.Item>
-          <Form.Item
-            name="Số phòng ngủ"
-            label="Số phòng ngủ"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <InputNumber />
-          </Form.Item>
-          <Form.Item
-            name="Số tầng"
-            label="Số tầng"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <InputNumber />
-          </Form.Item>
-          <Form.Item
-            name="Tỉnh/Thành phố"
-            label="Tỉnh/Thành phố"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Select
-              placeholder="Select a option and change input text above"
-              //   onChange={onGenderChange}
-              allowClear
+      <Row gutter={24}>
+        <Col span={8}>
+          <TitlePredict>Dự đoán giá chung cư</TitlePredict>
+          <PredictBox>
+            <Form
+              layout="vertical"
+              form={form}
+              name="control-hooks"
+              onFinish={onFinish}
+              style={{
+                maxWidth: 500,
+              }}
             >
-              <Option value="male">Hà Nội</Option>
-              <Option value="female">Hồ Chí Minh</Option>
-              <Option value="other">Bắc Ninh</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item
-            name="Quận/Huyện"
-            label="Quận/Huyện"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Select
-              placeholder="Select a option and change input text above"
-              //   onChange={onGenderChange}
-              allowClear
-            >
-              <Option value="male">Hà Nội</Option>
-              <Option value="female">Hồ Chí Minh</Option>
-              <Option value="other">Bắc Ninh</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item
-            name="Phường/Xã"
-            label="Phường/Xã"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Select
-              placeholder="Select a option and change input text above"
-              //   onChange={onGenderChange}
-              allowClear
-            >
-              <Option value="male">Hà Nội</Option>
-              <Option value="female">Hồ Chí Minh</Option>
-              <Option value="other">Bắc Ninh</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Dự đoán
-            </Button>
-            <Button htmlType="button" onClick={onReset}>
-              Đặt lại
-            </Button>
-          </Form.Item>
-        </Form>
-      </LayoutPredict>
+              <Row gutter={24}>
+                <Col span={12}>
+                  <Form.Item
+                    name="Diện tích"
+                    label="Diện tích"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                    labelCol={{ style: { color: "blue !important" } }}
+                  >
+                    <InputNumber style={{ width: "160px" }} min={1} />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    name="Số phòng ngủ"
+                    label="Số phòng ngủ"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <InputNumber style={{ width: "160px" }} min={1} />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={24}>
+                <Col span={12}>
+                  <Form.Item
+                    name="Số tầng"
+                    label="Số tầng"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <InputNumber style={{ width: "160px" }} min={1} />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    name="Tỉnh/Thành phố"
+                    label="Tỉnh/Thành phố"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <Select
+                      style={{
+                        width: 160,
+                      }}
+                      allowClear
+                      value={selectedCity || undefined}
+                      onChange={handleCityChange}
+                      onClear={() => {
+                        setSelectedCity("");
+                        setSelectedWard("");
+                        setSelectedDistrict("");
+                      }}
+                    >
+                      {cities.map((city) => (
+                        <Option key={city.code} value={city.code}>
+                          {city.name}
+                        </Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row gutter={24}>
+                <Col span={12}>
+                  <Form.Item
+                    name="Quận/Huyện"
+                    label="Quận/Huyện"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <Select
+                      style={{
+                        width: 160,
+                      }}
+                      allowClear
+                      value={selectedDistrict || undefined}
+                      onChange={handleDistrictChange}
+                      onClear={() => {
+                        setSelectedDistrict("");
+                        setSelectedWard("");
+                      }}
+                    >
+                      {districts.map((district) => (
+                        <Option key={district.code} value={district.code}>
+                          {district.name}
+                        </Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    name="Phường/Xã"
+                    label="Phường/Xã"
+                    rules={[
+                      {
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <Select
+                      value={selectedWard || undefined}
+                      style={{
+                        width: 160,
+                      }}
+                      allowClear
+                      onChange={handleWardChange}
+                      onClear={() => setSelectedWard("")}
+                    >
+                      {wards.map((ward) => (
+                        <Option key={ward.code} value={ward.code}>
+                          {ward.name}
+                        </Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+
+              <Form.Item>
+                <Row gutter={24}>
+                  <Col span={20}>
+                    <ButtomView type="primary" htmlType="submit">
+                      Dự đoán
+                    </ButtomView>
+                  </Col>
+                  <Col span={4}>
+                    <ButtomView
+                      type="primary"
+                      icon={<ReloadOutlined />}
+                      size="small"
+                      onClick={onReset}
+                    />
+                  </Col>
+                </Row>
+              </Form.Item>
+            </Form>
+          </PredictBox>
+          <ResultBox>
+            <span style={{ fontSize: "16px" }}>
+              Giá dự đoán: 2 tỷ ~ 35 triệu/m²
+            </span>
+          </ResultBox>
+        </Col>
+        <Col span={16}>
+          <TitlePredict>Bạn có thể quan tâm</TitlePredict>
+          <RecommentBox></RecommentBox>
+        </Col>
+      </Row>
     </Fragment>
   );
 };
--- real-portal/real-portal/src/components/Predict/styles.js
+++ real-portal/real-portal/src/components/Predict/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { Button } from "antd";
 export const LayoutPredict = styled.div`
   margin: 0 auto;
   max-width: 680px;
@@ -14,4 +14,52 @@ export const TitlePredict = styled.div`
   letter-spacing: -0.2px;
   color: #2c2c2c;
   display: block;
+  margin-bottom: 16px;
+`;
+export const PredictBox = styled.div`
+  border: 1px solid rgb(231 231 231);
+  border-radius: 0 4px 4px 4px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  // margin: 0 auto;
+  max-width: 450px;
+  // padding: 40px;
+  position: relative;
+  padding-left: 36px;
+  padding-top: 20px;
+  padding-bottom: 10px;
+`;
+export const ResultBox = styled.div`
+  border: 1px solid rgb(231 231 231);
+  border-radius: 0 4px 4px 4px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  // margin: 0 auto;
+  max-width: 450px;
+  // padding: 40px;
+  // position: relative;
+  padding: 16px;
+  margin-top: 20px;
+`;
+export const RecommentBox = styled.div`
+  border: 1px solid rgb(231 231 231);
+  border-radius: 0 4px 4px 4px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  // width: 800px;
+  height: 580px;  
+  padding: 16px;
+  margin-top: 20px;
+`;
+export const ButtomView = styled(Button)`
+  color: #fff;
+  background: #009ba1;
+  border: 1px solid #009ba1;
+  font-family: "Lexend Medium", Roboto, Arial !important;
+  font-size: 14px;
+  font-weight: normal !important;
+  letter-spacing: -0.2px;
+  border-radius: 4px;
+  &:hover {
+    color: #fff !important;
+    background: #1dbabf !important;
+    border: 1px solid #1dbabf !important;
+  }
 `;
